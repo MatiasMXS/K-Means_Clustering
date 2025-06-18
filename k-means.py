c91ae059d7d3f8fb5e50ad2422cb9e78ca6caa1f
MatiasMXS <130255970+MatiasMXS@users.noreply.github.com>
2025-06-18 10:48:47 -0300

    Subida de Codigo completo

--- k-means.py
+++ k-means.py
@@ -2,28 +2,40 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("iris.csv")  # sin header=None
+# Leer el dataset con codificación compatible
+data = pd.read_csv("Iris.csv")  # o "cp1252"
 
+# Inicializar 2 centroides (SepalWidthCm, PetalLengthCm)
 Centroides=np.array([
     [5.9,1.9],
-    [4.3,1.6],
-    [1.7,0.4],
+    [4.2,1.6],
+    [6.7,2.2],
     ])
 
 X = data[["PetalLengthCm", "PetalWidthCm"]].values
 labels = np.zeros(X.shape[0])
-for iter in range(10):  # o más si querés
+iteracion_final = 0
+for iter in range(100):  # máximo de 100 iteraciones por seguridad
+    centroides_previos = Centroides.copy()
+
     # Asignar cada punto al centroide más cercano
     for i in range(len(X)):
         distancias = [np.sqrt((Centroides[j][0] - X[i][0])**2 + (Centroides[j][1] - X[i][1])**2) for j in range(3)]
         labels[i] = np.argmin(distancias)
 
-    # Actualizar centroides con los promedios de sus puntos asignados
+    # Actualizar centroides
     for j in range(3):
         puntos_del_cluster = X[labels == j]
         if len(puntos_del_cluster) > 0:
             Centroides[j] = np.mean(puntos_del_cluster, axis=0)
 
+    # Cortar si los centroides no cambian
+    if np.allclose(Centroides, centroides_previos):
+        print(f"Convergió en la iteración {iter+1}")
+        break
+
+for j in range(2):
+    print(f"Cordenadas del centroide {j+1} x={Centroides[j][0]} Y={Centroides[j][1]}")
 # Colores para cada cluster
 colores = ['blue', 'green', 'red']
 
@@ -41,4 +53,3 @@ plt.title("K-Means Clustering - Iris Dataset")
 plt.legend()
 plt.grid(True)
 plt.show()
-
